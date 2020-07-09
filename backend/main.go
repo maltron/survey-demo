777530056bfb84102e0783f70e9a6beaf7a4357c
Mauricio "Maltron" Leal <maltron@gmail.com>
2020-07-08 23:35:25 -0300

    Added Database capability

--- backend/main.go
+++ backend/main.go
@@ -5,10 +5,24 @@ import (
 	"net/http"
 	"github.com/gorilla/mux"
 	"github.com/maltron/survey-demo/backend/users"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var (
+	database *DB
+	databaseError error 
 )
 
 func main() {
 	log.Println("SURVEY DEMO: Starting at Port :8080")
+
+	// Connecting to the database 
+	database, databaseError = sql.Open("mysql", "mauricio:maltron@tcp(127.0.0.1:3306)/survey")
+	if databaseError != nil {
+		panic(databaseError.Error())
+	}
+	defer database.Close()
 
 	router := mux.NewRouter()
 	router.HandleFunc("/user", users.GetUsers).Methods("GET")
