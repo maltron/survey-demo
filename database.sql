2f8e9f567b2d3c9a42887e5ec9ff30515de5ebdb
Mauricio "Maltron" Leal <maltron@gmail.com>
2020-07-10 15:18:42 -0300

    Added some data into the SQL

--- database.sql
+++ database.sql
@@ -1,2 +1,17 @@
 # create database survey character set utf8 collate utf8_general_ci;
+drop table if exists survey_demo cascade;
 create table if not exists survey_user(ID int not null auto_increment, firstName varchar(50) not null, lastName varchar(50) not null, unique(firstName, lastName), primary key(ID));
+# Adding some users for testing
+insert into survey_user(firstName, lastName) values('John','Smith');
+insert into survey_user(firstName, lastName) values('Ada','Lovelace');
+insert into survey_user(firstName, lastName) values('Steve','Jobs');
+insert into survey_user(firstName, lastName) values('Mark','Hamill');
+insert into survey_user(firstName, lastName) values('Zod','Destroyer');
+insert into survey_user(firstName, lastName) values('Sandra','Bullock');
+insert into survey_user(firstName, lastName) values('William','Smith');
+insert into survey_user(firstName, lastName) values('Keanu','Reeves');
+insert into survey_user(firstName, lastName) values('Han','Solo');
+insert into survey_user(firstName, lastName) values('Harry','Porter');
+insert into survey_user(firstName, lastName) values('Donald','Trump');
+insert into survey_user(firstName, lastName) values('Serena','Williams');
+insert into survey_user(firstName, lastName) values('Cassandra','Clark');
