2517882410a4f01dd85b9a35796fb740a2b59fc6
Mauricio "Maltron" Leal <maltron@gmail.com>
2020-08-18 09:30:08 -0300

    Distinguish between Speaker and Attendee users and added answers from attendees

--- database/database.sql
+++ database/database.sql
@@ -1,40 +1,35 @@
 # create database survey character set utf8 collate utf8_general_ci;
 alter database survey character set utf8mb4 collate utf8mb4_unicode_ci;
-drop table if exists survey_demo cascade;
-create table if not exists survey_user(ID int not null auto_increment, firstName varchar(50) not null, lastName varchar(50) not null, unique(firstName, lastName), primary key(ID)) default charset utf8mb4 collate utf8mb4_unicode_ci;
-delete from survey_user;
-# Adding some users for testing
-insert into survey_user(firstName, lastName) values('Anão', 'Caçamba');
-insert into survey_user(firstName, lastName) values('你好', '世界');
-insert into survey_user(firstName, lastName) values('John','Smith');
-insert into survey_user(firstName, lastName) values('Ada','Lovelace');
-insert into survey_user(firstName, lastName) values('Steve','Jobs');
-insert into survey_user(firstName, lastName) values('Mark','Hamill');
-insert into survey_user(firstName, lastName) values('Zod','Destroyer');
-insert into survey_user(firstName, lastName) values('Sandra','Bullock');
-insert into survey_user(firstName, lastName) values('William','Smith');
-insert into survey_user(firstName, lastName) values('Keanu','Reeves');
-insert into survey_user(firstName, lastName) values('Han','Solo');
-insert into survey_user(firstName, lastName) values('Harry','Porter');
-insert into survey_user(firstName, lastName) values('Donald','Trump');
-insert into survey_user(firstName, lastName) values('Serena','Williams');
-insert into survey_user(firstName, lastName) values('Cassandra','Clark');
+
+# Survey Speaker
+create table if not exists survey_speaker(ID int not null auto_increment, username varchar(150) not null, email varchar(150) not null, password varchar(10) not null, unique(username), unique(email), primary key(ID));
+delete from survey_speaker; 
+
 # Survey
 create table if not exists survey(ID int not null auto_increment, name varchar(150) not null, primary key(ID));
+delete from survey; 
+
+# Speaker has Surveys
+create table if not exists survey_speaker_has_surveys(speakerID int not null, surveyID int not null, foreign key(speakerID) references survey(ID), foreign key(surveyID) references survey(ID), unique(speakerID, surveyID));
+delete from survey_speaker_has_surveys;
+
 # Questions and Answers
-create table if not exists survey_question(ID int not null auto_increment, question varchar(255) not null, primary key(ID));
+create table if not exists survey_question(ID int not null auto_increment, question varchar(255) not null, timer int not null, points int not null, primary key(ID));
+delete from survey_question; 
+
 create table if not exists survey_answer(ID int not null auto_increment, answer varchar(255) not null, is_correct bool not null default false, primary key(ID));
-create table if not exists survey_question_has_answers(questionID int not null, answerID int not null, foreign key(questionID) references survey_question(ID), foreign key(answerID) references survey_answer(ID), unique(questionID, answerID));
-create table if not exists survey_has_questions(surveyID int not null, questionID int not null, foreign key(surveyID) references survey(ID), foreign key(questionID) references survey_question(ID), unique(surveyID, questionID));
-delete from survey_question_has_answers; 
-delete from survey_has_questions;
-delete from survey;
 delete from survey_answer; 
-delete from survey_question;
+
+create table if not exists survey_question_has_answers(questionID int not null, answerID int not null, foreign key(questionID) references survey_question(ID), foreign key(answerID) references survey_answer(ID), unique(questionID, answerID));
+delete from survey_question_has_answers; 
+
+create table if not exists survey_has_questions(surveyID int not null, questionID int not null, foreign key(surveyID) references survey(ID), foreign key(questionID) references survey_question(ID), unique(surveyID, questionID));
+delete from survey_has_questions; 
+
 # Sample Data
 insert into survey(ID, name) values(1, 'Capitals of the World');
 
-insert into survey_question(ID, question) values(1, 'What is the capital of China ?');
+insert into survey_question(ID, question, timer, points) values(1, 'What is the capital of China ?', 25, 50);
 insert into survey_has_questions(surveyID, questionID) values(1, 1); 
 insert into survey_answer(ID, answer) values(1, 'San Diego');
 insert into survey_answer(ID, answer, is_correct) values(2, 'Beijing', true);
@@ -45,7 +40,7 @@ insert into survey_question_has_answers(questionID, answerID) values(1,2);
 insert into survey_question_has_answers(questionID, answerID) values(1,3);
 insert into survey_question_has_answers(questionID, answerID) values(1,4);
 
-insert into survey_question(ID, question) values(2, 'What is the Capital of Bolivia ?');
+insert into survey_question(ID, question, timer, points) values(2, 'What is the Capital of Bolivia ?', 25, 60);
 insert into survey_has_questions(surveyID, questionID) values(1, 2);
 insert into survey_answer(ID, answer) values(5, 'Lima');
 insert into survey_answer(ID, answer, is_correct) values(6, 'La Paz', true);
@@ -56,7 +51,7 @@ insert into survey_question_has_answers(questionID, answerID) values(2, 6);
 insert into survey_question_has_answers(questionID, answerID) values(2, 7);
 insert into survey_question_has_answers(questionID, answerID) values(2, 8);
 
-insert into survey_question(ID, question) values(3, 'What is the Capital of Ghana ?');
+insert into survey_question(ID, question, timer, points) values(3, 'What is the Capital of Ghana ?', 25, 50);
 insert into survey_has_questions(surveyID, questionID) values(1, 3);
 insert into survey_answer(ID, answer) values(9, 'Lisbon');
 insert into survey_answer(ID, answer) values(10, 'Seville');
@@ -72,3 +67,26 @@ select * from survey_question question join survey_has_questions sq on question.
 
 # Query: Select Answers for a given Question within a Survey
 select * from survey_answer answer join survey_question_has_answers qa on answer.ID = qa.answerID join survey_has_questions sq on sq.questionID = qa.questionID where sq.surveyID = 1 and sq.questionID = 1;
+
+# Survey Attendees
+create table if not exists survey_attendee(ID int not null auto_increment, firstName varchar(50) not null, lastName varchar(50) not null, email varchar(150) not null, unique(firstName, lastName), unique(email), primary key(ID)) default charset utf8mb4 collate utf8mb4_unicode_ci;
+delete from survey_attendee;
+# Adding some users for testing
+insert into survey_attendee(firstName, lastName, email) values('Anão', 'Caçamba', 'anao@cacamba.com');
+insert into survey_attendee(firstName, lastName, email) values('你好', '世界', 'hello@world.com');
+insert into survey_attendee(firstName, lastName, email) values('John','Smith', 'john@smith.com');
+insert into survey_attendee(firstName, lastName, email) values('Ada','Lovelace', 'ada@lovelace.com');
+insert into survey_attendee(firstName, lastName, email) values('Steve','Jobs', 'steve@jobs.com');
+insert into survey_attendee(firstName, lastName, email) values('Mark','Hamill', 'mark@hamill.com');
+insert into survey_attendee(firstName, lastName, email) values('Zod','Destroyer', 'zod@destroyer.com');
+insert into survey_attendee(firstName, lastName, email) values('Sandra','Bullock', 'sandra@bullock.com');
+insert into survey_attendee(firstName, lastName, email) values('William','Smith', 'william@smith.com');
+insert into survey_attendee(firstName, lastName, email) values('Keanu','Reeves', 'keanu@reeves.com');
+insert into survey_attendee(firstName, lastName, email) values('Han','Solo', 'han@solo.com');
+insert into survey_attendee(firstName, lastName, email) values('Harry','Porter', 'harry@porter.com');
+insert into survey_attendee(firstName, lastName, email) values('Donald','Trump', 'donald@trump.com');
+insert into survey_attendee(firstName, lastName, email) values('Serena','Williams', 'serena@williams.com');
+insert into survey_attendee(firstName, lastName, email) values('Cassandra','Clark', 'cassandra@clark.com');
+
+# Survey Attendees Answers
+create table survey_attendee_answers(ID int not null auto_increment, attendeeID int not null, points int not null, foreign key(attendeeID) references survey_attendee(ID), primary key(ID));
